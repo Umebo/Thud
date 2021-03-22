454dd4091440116c10317610bc6742d48f99b744
Umebo <igorniemiec00@gmail.com>
2021-03-22 20:45:10 +0100

    redefining pawns storage to dict new function -> creating pawns from positions list app run silenced some auxiliary prints minor changes to kv file ------------------------------------- still no concept for tracking moves separate functions for game setup in another package?

--- elements.py
+++ elements.py
@@ -5,14 +5,31 @@ class Pawn():
         self.dim_x = dim_x
         self.dim_y = dim_y
 
+    def __str__(self):
+        return 'dwarf: [{},{}]'.format(self.dim_x, self.dim_y)
+
     def move(self, dim_x, dim_y):
         self.dim_x = dim_x
         self.dim_y = dim_y
 
 
 class Dwarf(Pawn):
+    symbol = "D"
+    is_dwarf = True
     pass
 
 
 class Troll(Pawn):
+    symbol = "T"
+    is_troll = True
     pass
+
+
+def fill_pawn_list(positions, pawn_list, pawn_type):
+    counter = 0
+    for i in positions:
+        pawn_list.append({
+            'id': pawn_type.symbol + str(counter),
+            'dwarf': pawn_type(i[0], i[1])
+        })
+        counter += 1
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import numpy as np
 import elements as el
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+
+dwarf_positions = [[1, 1], [7, 3], [2, 1]]
+dwarf_pawns = []
+
+el.fill_pawn_list(dwarf_positions, dwarf_pawns, el.Dwarf)
+
+
+class Board():
+    def __init__(self):
+        self.board = np.zeros((15, 15))
+
+    def show(self):
+        print(self.board)
+
+    def fill_board(self, pawns):
+        for pawn in pawns:
+            self.board[pawn.dim_x, pawn.dim_y] = 'D'
 
 
 class ThudGame(BoxLayout):
@@ -15,5 +30,13 @@ class ThudApp(App):
         return ThudGame()
 
 
+'''
 if __name__ == '__main__':
     ThudApp().run()
+'''
+
+board = Board()
+# board.show()
+print(dwarf_pawns)
+print(dwarf_pawns[0]['id'], dwarf_pawns[0]['dwarf'].dim_x)
+print(len(dwarf_pawns))
